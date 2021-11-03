ecbbebd2c0e2d8c650288cc9d427dbfb45e731a1
ralexrivero <ralexrivero@gmail.com>
2021-11-03 02:14:49 -0300

    clean files, add compilation script

---
+++ c/compilation.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Compilation script for recursion
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 0-main_pow_recursion.c 0-pow_recursion.c -o 0-recursion
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 0-main_pow_recursion_argv.c 0-pow_recursion.c -o 0-recursion_argv
